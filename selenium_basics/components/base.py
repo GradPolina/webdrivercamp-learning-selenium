478c5a03f820cfc390c09766b43abfd85c9d7980
GradPolina <Gradinar_polina@yahoo.com>
2024-06-21 19:28:22 -0500

    Done tasks from 0 to 1

--- selenium_basics/components/base.py
+++ selenium_basics/components/base.py
@@ -1,1 +1,10 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+class Base:
+    def __init__(self, driver):
+        self.driver = driver
+    def click(self, locator):
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator)).click()
+
 
